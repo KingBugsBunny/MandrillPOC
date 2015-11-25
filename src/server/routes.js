633a8646f486858534d03d100508eace8921654b
RobertCawBrush <robert.brush@brieBug.com>
2015-11-25 12:57:41 -0700

    mandrill api use added to route file

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,10 +1,29 @@
 var router = require('express').Router();
 var four0four = require('./utils/404')();
 var data = require('./data');
+var mandrill = require('mandrill-api/mandrill');
 
 router.get('/people', getPeople);
 router.get('/person/:id', getPerson);
 router.get('/*', four0four.notFoundMiddleware);
+
+//create instance of Mandrill and designate API key
+var m = new mandrill.Mandrill('fRpV-9-cdN04u7RHKn9N4w');
+//Establish parameters here, refer to documentation to see all available parameters (there's a large amount)
+var params = {
+    'message': {
+        'from_email':'robertcawbrush@gmail.com',
+        'to':[{'email':'robert.brush@briebug.com'}],
+        'subject': 'Sending a text email from the Mandrill API',
+        //this parameter can be html or text and is the actual message sent
+        'html': 'This is an example email sent from MandrillPOC to show mandrill use',
+        //this parameter will default to a text message if the users email service doesn't support html
+        'autotext': 'true',
+        //mandrill offers analytics through their console to track opened URLs as well as clicked html
+        'tracks_open': 'true',
+        'track_clicks': 'true',
+    }
+};
 
 module.exports = router;
 
@@ -12,6 +31,14 @@ module.exports = router;
 
 function getPeople(req, res, next) {
     res.status(200).send(data.people);
+
+    //using mandrill send mail
+    m.messages.send(params, function(res) {
+        console.log(res);
+    }, function(err) {
+        console.log(err);
+    });
+
 }
 
 function getPerson(req, res, next) {
